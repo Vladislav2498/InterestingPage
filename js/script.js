2ed5a4a3d04f0866ba0d42f29fedca3648d57207
Vladyslav Chmutov <vladislav2498@gmail.com>
2022-05-06 03:19:19 +0300

    06.05.2022 commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 "use strict"
 
+//our services
+
+const handleClick = (event) => {
+    document.querySelector('.our-services-menu-item.active').classList.remove('active');
+    event.target.classList.add('active')
+
+    const category = event.target.dataset.category;
+    document.querySelector(`.our-services-information.active`).classList.remove('active');
+    document.querySelector(`[data-set="${category}"]`).classList.add('active');
+}
+
+    const btnCollection = document.querySelectorAll('.our-services-menu li');
+    btnCollection.forEach(btn => btn.addEventListener('click' , handleClick));
 
 
